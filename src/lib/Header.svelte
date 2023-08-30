45130139ba3b825d27a0268c72ee5206f8accb86
Kyohei Uto <im@kyoheiu.dev>
2023-08-30 09:54:21 +0900

    Clear input after submission

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
+	import { afterNavigate } from "$app/navigation";
+
+	afterNavigate(() => {
+		let form: HTMLFormElement | null = document.forms[0];
+		if (form) {
+			form.reset();
+		}
+	})
 </script>
 
 <div class="sticky z-50 top-0 flex w-full flex-col items-center bg-itembackground text-sm shadow">
 	<div class="flex h-12 w-64 items-baseline pt-2 sm:w-120 md:w-144">
-		<a class=" no-underline" href="/">name</a>
+		<a class=" no-underline" href="/">carbon</a>
 		&nbsp; &nbsp;
 		<a
 			href="/item"
@@ -13,7 +21,7 @@
 		>
 		&nbsp; &nbsp;
 		<div class="ml-auto">
-			<form action="/search">
+			<form id="search" action="/search">
 				<input name="q" placeholder="Search" class="w-32 rounded-full bg-background px-3 py-1" />
 			</form>
 		</div>
