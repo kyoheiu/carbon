091e72f42b85004e24385076546d79ac700a66c1
Kyohei Uto <im@kyoheiu.dev>
2023-08-31 10:47:31 +0900

    Refactor color

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,6 @@ export default {
 			further: '#9ca3af', // gray-400
 			basecolor: '#2563eb', // blue-600
 			baseborder: '#60a5fa', // blue-400
-			new: '#16a34a', // green-600
-			newborder: '#4ade80', // green-400
 			hovertitle: '#2563eb', // blue-600
 			warning: '#b91c1c', // red-700
 			saved: '#1e40af' // blue-800
