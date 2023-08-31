f7371bdceacc5ef6e2f413314fd04b708996ce68
Kyohei Uto <im@kyoheiu.dev>
2023-08-31 11:02:43 +0900

    Change `new` button color

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -10,13 +10,13 @@
 </script>
 
 <div class="sticky z-50 top-0 flex w-full flex-col items-center bg-itembackground text-sm shadow">
-	<div class="flex h-12 w-64 items-baseline pt-2 sm:w-120 md:w-144">
-		<a class=" no-underline" href="/">carbon</a>
+	<div class="flex h-12 w-64 items-center pt-2 sm:w-120 md:w-144">
+		<a class="no-underline" href="/"><img class="ml-1" src="logo.png" alt="carbon" width="60" /></a>
 		&nbsp; &nbsp;
 		<a
 			href="/item"
 			title="add a new file"
-			class="no-underline h-6 bg-basecolor px-2 py-1 text-xs font-semibold text-lightbuttontext"
+			class="no-underline h-6 bg-itembackground border text-basecolor border-baseborder px-2 py-1 text-xs font-semibold"
 			>+New</a
 		>
 		&nbsp; &nbsp;
