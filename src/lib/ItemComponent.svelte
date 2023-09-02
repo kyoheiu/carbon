6a13922338427b8dfa5885db25293025c4b93824
Kyohei Uto <im@kyoheiu.dev>
2023-09-03 06:31:41 +0900

    Fix margin between title and desc

--- src/lib/ItemComponent.svelte
+++ src/lib/ItemComponent.svelte
@@ -20,11 +20,11 @@
 				>{item.name}</a
 			>
 			{#if item.desc}
-				<div class="mt-4 w-40 truncate text-desc sm:w-72 md:w-96">
+				<div class="mt-3 w-40 truncate text-desc sm:w-72 md:w-96">
 					{item.desc}
 				</div>
 			{:else}
-				<div class="mt-4 w-40 truncate italic text-desc sm:w-72 md:w-96">No contents.</div>
+				<div class="mt-3 w-40 truncate italic text-desc sm:w-72 md:w-96">No contents.</div>
 			{/if}
 		</div>
 		<span class="ml-2 w-12 text-right text-sm text-subtle sm:w-20">{toDuration(item.modified)}</span
