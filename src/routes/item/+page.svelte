e563d6faab384cec34b78d1ef5b10d3d4e68e2cb
Kyohei Uto <im@kyoheiu.dev>
2023-09-12 18:48:42 +0900

    Fix red dot position

--- src/routes/item/+page.svelte
+++ src/routes/item/+page.svelte
@@ -159,7 +159,7 @@
 					>
 						Save
 						<div
-							class="rounded-full absolute bottom-4 left-14 w-3 h-3 bg-warning border-2 border-background"
+							class="rounded-full absolute bottom-4 left-12 w-3 h-3 bg-warning border-2 border-background"
 						/>
 					</button>
 				{:else}
