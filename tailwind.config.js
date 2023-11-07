064def6d82de774a3c61f0530396cb61a02167de
Kyohei Uto <im@kyoheiu.dev>
2023-11-08 06:15:13 +0900

    Add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,5 +42,8 @@ export default {
 			}
 		}
 	},
-	plugins: [require('@tailwindcss/typography')]
+	plugins: [require('@tailwindcss/typography'), require("daisyui")],
+	daisyui: {
+		themes: ["cmyk"],
+	},
 };
