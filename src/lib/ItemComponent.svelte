85992fe68dac55a569780745d95a49c68ee3261f
Kyohei Uto <im@kyoheiu.dev>
2023-11-25 07:04:31 +0900

    Remove card class

--- src/lib/ItemComponent.svelte
+++ src/lib/ItemComponent.svelte
@@ -13,7 +13,7 @@
 </script>
 
 {#if !hidden}
-	<div class="card w-full px-2 py-4 sm:px-4 sm:w-56 md:w-44 bg-surface-100 mx-auto">
+	<div class="!w-full rounded px-2 py-4 sm:px-4 sm:w-56 md:w-44 bg-surface-100 mx-auto">
 		<div class="w-full overflow-ellipsis line-clamp-1 break-all mb-4">
 			<a
 				class="no-underline text-lg text-tertiary-700 font-semibold"
