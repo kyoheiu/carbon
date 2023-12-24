88828a5e50c5d85056134cd6527a4f0d9abe8dc8
Kyohei Uto <im@kyoheiu.dev>
2023-12-24 18:01:41 +0900

    Add modal to header

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,23 +1,47 @@
+import { useCallback, useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
-const handleClick = async (e: any) => {
-  const res = await fetch("http://localhost:3000/items", {
-    method: "POST",
-  });
-  if (!res.ok) {
-    console.error("Post failed.");
-  } else {
-    const newFile = await res.text();
-    window.location.href = `/items/${newFile}`;
-  }
-};
-
 export const Root = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [newFile, setNewFile] = useState("");
+
+  const handleClick = () => {
+    console.log("click");
+    setShowModal(true);
+  };
+
+  const handleCreate = async (e: any) => {
+    const res = await fetch("http://localhost:3000/items", {
+      method: "POST",
+      body: newFile,
+    });
+    if (!res.ok) {
+      console.error("Post failed.");
+    } else {
+      const newFile = await res.text();
+      window.location.href = `/items/${newFile}`;
+    }
+    setNewFile("");
+  };
+
   return (
     <>
       <header>
         <a href="/">carbon</a>
         <button onClick={handleClick}>New</button>
+        {showModal && (
+          <>
+            <button onClick={() => setShowModal(false)}>close</button>
+            <div>
+              <input
+                type="text"
+                value={newFile}
+                onChange={(e) => setNewFile(() => e.target.value)}
+              />
+              <button onClick={handleCreate}>Create</button>
+            </div>
+          </>
+        )}
       </header>
       <div>
         <Outlet />
