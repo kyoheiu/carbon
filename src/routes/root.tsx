c73b7a9b605750424d69b5c59a04b28d7ef986db
Kyohei Uto <im@kyoheiu.dev>
2023-12-24 14:08:48 +0900

    Add button to create new file

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,10 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+
+const handleClick = async (e: any) => {
+  const res = await fetch("http://localhost:3000/items", {
+    method: "POST",
+  });
+  if (!res.ok) {
+    console.error("Post failed.");
+  } else {
+    const newFile = await res.text();
+    window.location.href = `/items/${newFile}`;
+  }
+};
 
 export const Root = () => {
   return (
     <>
       <header>
         <a href="/">carbon</a>
+        <button onClick={handleClick}>New</button>
       </header>
       <div>
         <Outlet />
